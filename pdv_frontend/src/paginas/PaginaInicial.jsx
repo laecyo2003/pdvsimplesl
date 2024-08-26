6408ef14aebb593612212165ea2300ba75c0b49f
laecyo2003 <laecyo12345@gmail.com>
2024-08-26 17:01:12 -0300

    parte das vendas do pdv

--- pdv_frontend/src/paginas/PaginaInicial.jsx
+++ pdv_frontend/src/paginas/PaginaInicial.jsx
@@ -7,7 +7,7 @@ const PaginaInicial = () => {
 <LayoutPrincipal>
     <div className='conatiner mt-3 mb-4'>
                     <div className='p-5 mt-4 rounded-lg mb-4 text-center titulo-aplicacao'>
-                        <h1 style={{fontWeight:'bold'}}> PDV Simples para pequenos negócios </h1>
+                        <h1 style={{fontSize:'80px',fontWeight:'bold'}}> PDV SIMPLES PARA PEQUENOS NEGÓCIOS </h1>
                     </div>
                 </div>
                 <div className='conatiner-botao1 mb-4 text-center' >
@@ -19,32 +19,52 @@ const PaginaInicial = () => {
 
 
                 <div className='informacoes mb-3'>
-                    <h1> INFORMAÇÕES SOBRE O PROJETO </h1>
+                    <h1 style={{fontSize: '50px', textAlign: 'center', fontWeight: 'bold'}}> INFORMAÇÕES SOBRE O PROJETO </h1>
                 </div>
                 <div className='versao-projeto mb-3'>
-                    <h2> Versão do projeto </h2>
-                    <h3> v0.0.1-beta </h3>
+                    <h2 style={{fontSize: '30px', fontWeight: 'bold'}}> Versão do projeto </h2>
+                    <p1 style={{fontSize: '24px'}}> v0.0.1-beta </p1>
 
                 </div>
 
                 <div className='objetivo-projeto mb-3'>
-                    <h2> Objetivo do projeto </h2>
-                    <p1> Esta aplicação WEB possui como finalidade a conclusão da disciplina ECT3104 - Lógica de Programação - (LOP), ministrada pelo professor ORIVALDO VIEIRA DE SANTANA JUNIOR. Trata-se de um Ponto de Venda destinado a pequenos negócios. Construído com banco de dados JSON, utilizando-se de um servidor JSON como backend e React como frontend, a aplicação permite ao usuário: </p1>
-                    <li> Realizar vendas em um ponto de venda, o que é dar a opção de adicionar produtos a um carrinho, incluindo quantidades e ir somando cada produto até chegar no valor final de uma venda, a qual o usuário deve indicar que foi finalizada a partir do clique em um botão, o que lhe dará a opção de inserir o valor pago pelo cliente e, deste valor, a aplicação retornará o troco que deve ser devolvido ao cliente, encerrando a compra. </li>
-                    <li> Cadastrar novos produtos ao banco de dados JSON, inserindo o id do produto, que é definido por uma string do tipo "xxxxx", iniciando em "00001", o nome do produto, o preço do produto e uma foto do produto. </li>
+                    <h2 style={{fontSize: '35px',fontWeight: 'bold'}}> Objetivo do projeto </h2>
+                    <p1 style={{fontSize: '25px'}}> Esta aplicação WEB possui como finalidade a conclusão da disciplina ECT3104 - Lógica de Programação - (LOP), ministrada pelo professor ORIVALDO VIEIRA DE SANTANA JUNIOR. Trata-se de um Ponto de Venda destinado a pequenos negócios. Construído com banco de dados JSON, utilizando-se de um servidor JSON como backend e React como frontend, a aplicação permite ao usuário: </p1>
+                    <li style={{fontSize: '25px'}}> Realizar vendas em um ponto de venda, o que é dar a opção de adicionar produtos a um carrinho, incluindo quantidades e ir somando cada produto até chegar no valor final de uma venda, a qual o usuário deve indicar que foi finalizada a partir do clique em um botão, o que lhe dará a opção de inserir o valor pago pelo cliente e, deste valor, a aplicação retornará o troco que deve ser devolvido ao cliente, encerrando a compra; </li>
+                    <li style={{fontSize: '25px'}}> Cadastrar novos produtos ao banco de dados JSON, inserindo o id do produto, que é definido por uma string do tipo "xxxxx", iniciando em "00001", o nome do produto, o preço do produto e uma foto do produto. </li>
 
                 </div>
                 <div className='tecnologias-projeto mb-3'>
-                    <h2> Tecnologias utilizadas no projeto </h2>
-                    <h4> Frontend </h4>
-                    <li> React </li>
-                    <li> Bootstrap </li>
-                    <h4> Backend </h4>
-                    <li> NodeJs </li>
-                    <li> JSON SERVER </li>
+                    <h2 style={{fontSize: '35px',fontWeight: 'bold'}}> Tecnologias utilizadas no projeto </h2>
+                    <ul>
+
+                        <li>
+                            <h4 > Frontend </h4>
+                            <ul>
+                                <li> Bootstrap; </li>
+                                <li> NPM; </li>
+                                <li> React. </li>
+                            </ul>
+                        </li>
+                    </ul>
+                    
+
+                    <ul>
+                        <li> 
+                            <h4> Backend </h4>
+                            <ul>
+                                <li> JSON SERVER; </li>
+                                <li> NodeJS; </li>
+                                <li> NPM. </li>
+                            </ul>
+                        </li>
+                    </ul>
+
+
+
                 </div>
                 <div className='inspiracoes mb-3'>
-                    <h2> Inspiração para o projeto </h2>
+                    <h2 style={{fontSize: '35px',fontWeight: 'bold'}}> Inspiração para o projeto </h2>
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/8E7Xwy0lXlg?si=bS7NDgH4Jt6-8NC5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
                 </div>
--- pdv_frontend/src/paginas/PaginaPDV.jsx
+++ pdv_frontend/src/paginas/PaginaPDV.jsx
@@ -4,11 +4,38 @@ import axios from "axios"
 const PDV = () => {
     const [Produtos, setProdutos] = useState([]);
     const [EstaCarregando, setEstaCarregando] = useState(false);
+    const [Venda, setVenda] = useState([]);
     const CapturandoProdutos = async() => {
         setEstaCarregando(true);
         const resultado = await axios.get('Produtos');
         setProdutos(await resultado.data);
         setEstaCarregando(false);
+    }
+
+    const AdicionarProdutoAVenda = async(Produtos) =>{
+        // Checando se o produto adicionado à venda já existe
+        let EncontrarProdutoVenda =  await Venda.find(i =>{
+        return i.id === Produtos.id
+    });
+
+    if(EncontrarProdutoVenda){
+        // Encontrando produtos na venda e modificando quantidades
+        let NovaVenda = [];
+        let NovoProduto;
+        Venda.forEach()
+    }else{
+        let AdicionarProduto = {
+            ...Produtos,
+            'Quantidade': 1,
+            'Valor total': Produtos.preco,
+            }
+            setVenda([...Venda, AdicionarProduto])
+    } 
+    
+
+
+
+
     }
     
     useEffect(() => {
@@ -23,11 +50,11 @@ const PDV = () => {
         <div className='col-lg-8'>
             {EstaCarregando ? 'Carregando' : <div className='row'>
                 {Produtos.map((Produtos, chave) =>
-                <div chave={chave} className='col-lg-4'>
-                    <div className='border'>
+                <div chave={chave} className='col-sm-4'>
+                    <div className='border' onClick={() => AdicionarProdutoAVenda(Produtos)}>
                         <p> {Produtos.nome} </p>
-                        <img src={Produtos.imagem} className="image-fluid" alt={Produtos.nome} />
-                        <p> R${Produtos.preço} </p>
+                        <img src={Produtos.imagem} className="img-fluid" alt={Produtos.nome} />
+                        <p> R${Produtos.preco} </p>
                     </div>
                 </div>
             )}                
